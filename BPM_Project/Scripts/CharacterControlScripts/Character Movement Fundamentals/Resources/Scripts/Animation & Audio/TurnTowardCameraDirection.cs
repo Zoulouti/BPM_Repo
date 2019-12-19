c4cfabdcdfbae49527f66229a4191e37274a9724
Molina <lauramolina83@outlook.fr>
2019-12-19 22:31:08 +0100

    eh2

---
+++ BPM_Project/Scripts/CharacterControlScripts/Character Movement Fundamentals/Resources/Scripts/Animation & Audio/TurnTowardCameraDirection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script turns a gameobject toward the look direction of a chosen 'CameraController' component;
+public class TurnTowardCameraDirection : MonoBehaviour {
+
+	public CameraController cameraController;
+	Transform tr;
+
+	[SerializeField] Transform cameraTrans;
+
+	//Setup;
+	void Start () {
+		tr = transform;
+	}
+	
+	//Update;
+	void LateUpdate () {
+
+		if(!cameraController)
+			return;
+
+		//Calculate up and forwward direction;
+		// Vector3 _forwardDirection = cameraController.GetFacingDirection();
+		// Vector3 _upDirection = cameraController.GetUpDirection();
+		Vector3 _forwardDirection = cameraTrans.forward;
+		Vector3 _upDirection = cameraTrans.up;
+
+		//Set rotation;
+		tr.rotation = Quaternion.LookRotation(_forwardDirection, _upDirection);
+	}
+}
