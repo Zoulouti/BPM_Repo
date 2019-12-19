54149a465338b0ebda81e587f8ae237cfec5e0a6
Zoulouti <Pauldelaubadere@gmail.com>
2019-12-19 11:56:33 +0100

    bpm

--- BPM_Project/Assets/Scripts/WeaponAndBPM/WeaponBehaviour.cs
+++ BPM_Project/Assets/Scripts/WeaponAndBPM/WeaponBehaviour.cs
@@ -30,47 +30,34 @@ public class WeaponBehaviour : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Vector3 posToLookAt;
-            _BPMSystem.LoseBPM(allWeapons[activatedWeapon]._weaponLevel0.BPMCost);
-            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out _hit, Mathf.Infinity))
-            {
-
-                posToLookAt = _hit.point;
-
-
-                /*Debug.DrawLine(Camera.main.transform.position, _hit.transform.position, Color.blue, 10f);
-
-
-                if (_hit.collider.CompareTag("NoSpot"))
-                {
-                    _BPMSystem.GainBPM(10f);
-                }
-                else if (_hit.collider.CompareTag("WeakSpot"))
-                {
-                    _BPMSystem.GainBPM(20f);
-                }
-                else if (_hit.collider.CompareTag("ArmorSpot"))
-                {
-                    _BPMSystem.GainBPM(5f);
-                }
-                else if (_hit.collider.CompareTag("DestroyableObject"))
-                {
-                    _BPMSystem.GainBPM(5f);
-                    _BPMSystem.GainElectrarythmiePoints(5);
-                } */
-            }
-            else
-            {
-                posToLookAt = GetProjectilLookAt();
-            }
-
-            GameObject go = Instantiate(allWeapons[activatedWeapon]._weaponLevel0.bullet, firePoint.transform.position, firePoint.transform.rotation);
-            go.GetComponent<Projectile>().BPMSystem = _BPMSystem;
-            Enum m_projectilState = go.GetComponent<Projectile>().ProjectileType1 = Projectile.ProjectileType.Player;
-            go.transform.LookAt(posToLookAt);
-
-            OnShoot(m_projectilState);
+            OnPlayerShoot();
         }
+    }
+
+    void OnPlayerShoot()
+    {
+        Vector3 posToLookAt;
+
+        _BPMSystem.LoseBPM(allWeapons[activatedWeapon]._weaponLevel0.BPMCost);
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out _hit, Mathf.Infinity))
+        {
+            posToLookAt = _hit.point;
+        }
+        else
+        {
+            posToLookAt = GetProjectilLookAt();
+        }
+
+        GameObject go = Instantiate(allWeapons[activatedWeapon]._weaponLevel0.bullet, firePoint.transform.position, firePoint.transform.rotation);
+        Projectile projScript = go.GetComponent<Projectile>();
+
+        projScript.BPMSystem = _BPMSystem;
+        Enum m_projectilState = projScript.ProjectileType1 = Projectile.ProjectileType.Player;
+
+        go.transform.LookAt(posToLookAt);
+
+        OnShoot(m_projectilState);
     }
 
     public void OnShoot(Enum m_projectileType)
@@ -80,6 +67,6 @@ public class WeaponBehaviour : MonoBehaviour
 
     Vector3 GetProjectilLookAt()
     {
-        return Camera.main.transform.position + Camera.main.transform.forward * defaultDistance;
+        return Camera.main.transform.position + Camera.main.transform.forward * defaultDistance;            // Dirige le projectil dans la bonne direction (par rapport au reticule)
     }
 }
