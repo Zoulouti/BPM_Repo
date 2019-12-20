8363a36b94fc7af3d5470e479815fd15caaad4d8
Zoulouti <Pauldelaubadere@gmail.com>
2019-12-20 15:49:19 +0100

    BPM_Project/Assets/AmplifyShaderEditor.meta

---
+++ BPM_Project/Assets/testScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    Renderer rend;
+    public Shader shader;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+
+        // Use the Specular shader on the material
+        rend.material.shader = shader;
+    }
+
+    void Update()
+    {
+        // Animate the Shininess value
+        float chromapropability = Mathf.PingPong(Time.time, 1.0f);
+
+        Debug.Log(rend.material.GetFloat("chromapropability"));
+
+        rend.material.SetFloat("chromapropability", chromapropability);
+    }
+}
