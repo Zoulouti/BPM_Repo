6897a9d4f43fd674f1b89945ec5f11a5810f3675
JeremyPayre Laptop <jeremy.payre@live.fr>
2020-02-19 15:21:02 +0100

    Add PlayerAudioController.cs sound controller with PlayerAudioController.cs

--- BPM_Project/Assets/Scripts/Common/AudioController.cs
+++ BPM_Project/Assets/Scripts/Common/AudioController.cs
@@ -5,6 +5,23 @@ using UnityEngine;
 public class AudioController : MonoBehaviour
 {
     
+    [System.Serializable] public class Sounds
+    {
+        [Header("References")]
+        public AudioSource m_audioSource;
+
+        [Header("Sounds")]
+        public AudioClip[] m_sounds;
+
+        [Header("Volume")]
+        [Range(0, 1)] public float m_volume = 0.75f;
+        [Range(0, 0.5f)] public float m_volumeRandomizer = 0.2f;
+
+        [Header("Pitch")]
+        [Range(-3, 3)] public float m_pitch = 1;
+        [Range(0, 1.5f)] public float m_pitchRandomizer = 0.1f;
+    }
+
     protected AudioClip GetAudioFromArray(AudioClip[] audios)
     {
         if (audios.Length == 0)
---
+++ BPM_Project/Assets/Scripts/Level/Ground.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GroundType;
+
+public class Ground : MonoBehaviour
+{
+    
+    [SerializeField] GroundTypeEnum m_groundType = GroundTypeEnum.Stone;
+    public GroundTypeEnum GroundType { get => m_groundType; }
+    
+}
